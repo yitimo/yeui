558763e13b2181146ce7fda2e79bb56b70ec3f50
yitimo <635928008@qq.com>
2017-08-06 16:27:38 +0800

    edit export and npmignore rule

--- index.ts
+++ index.ts
@@ -1,6 +1,6 @@
 export { YINModule } from './src/yin/yin.module';
 export { YUPModule } from './src/yup/yup.module';
-export { YUPService, DialogConfig } from './src/yup/yup.service';
+export { YUPService, DialogConfig, LoadConfig, ToastConfig } from './src/yup/yup.service';
 export { AudioModule } from './src/yudio/module';
 export { AudioService } from './src/yudio/service';
 export { PlayData, Audio, PlayOrder } from './src/yudio/model';
