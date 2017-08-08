aab1be36ae5ea24d9f0a061b5722100c2eb194e0
yitimo <635928008@qq.com>
2017-08-08 18:03:14 +0800

    may have error when using aot compile, avoid it until fixed

--- src/yudio/module.ts
+++ src/yudio/module.ts
@@ -1,6 +1,5 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { AudioService } from './service';
 import { AudioComponent, AudioCtrlComponent } from './component';
 import { TimePipe } from './pipe';
 
