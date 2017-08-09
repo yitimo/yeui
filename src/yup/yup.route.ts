e17a95d1610935328bd4494dde8dd15cca68669a
yitimo <635928008@qq.com>
2017-08-09 10:56:06 +0800

    add .ts source code back to resolve module not declared when aot compile

--- src/yup/yup.route.ts
+++ src/yup/yup.route.ts
@@ -1,28 +1,22 @@
-import { Routes, RouterModule } from '@angular/router';
-import { NgModule } from '@angular/core';
+// import { Routes, RouterModule } from '@angular/router';
+// import { NgModule } from '@angular/core';
 
-import { HomeComponent } from './';
-import { Name2Component } from './';
-import { Name3Component } from './';
-import { Name4Component } from './';
-import { PageNotFoundComponent } from './';
+// import { HomeComponent } from './';
+// import { Name2Component } from './';
+// import { Name3Component } from './';
+// import { Name4Component } from './';
+// import { PageNotFoundComponent } from './';
 
-const routes: Routes = [
-    { path: '', component: HomeComponent },
-    { path: 'path2', component: Name2Component },
-    { path: 'path3', component: Name3Component },
-    { path: 'path4', component: Name4Component },
-    { path: '**', component: PageNotFoundComponent },
+// const routes: Routes = [
+//     { path: '', component: HomeComponent },
+//     { path: 'path2', component: Name2Component },
+//     { path: 'path3', component: Name3Component },
+//     { path: 'path4', component: Name4Component },
+//     { path: '**', component: PageNotFoundComponent }
+// ];
 
-    //{ path: 'path/:routeParam', component: MyComponent },
-    //{ path: 'staticPath', component: ... },
-    //{ path: '**', component: ... },
-    //{ path: 'oldPath', redirectTo: '/staticPath' },
-    //{ path: ..., component: ..., data: { message: 'Custom' }
-];
-
-@NgModule({
-    imports: [RouterModule.forChild(routes)],
-    exports: [RouterModule]
-})
-export class YupRoutingModule {}
+// @NgModule({
+//     imports: [RouterModule.forChild(routes)],
+//     exports: [RouterModule]
+// })
+// export class YupRoutingModule {}
