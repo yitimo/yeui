0d3d297f78b665f217fe28ae5c8d44ebeac32e70
yitimo <635928008@qq.com>
2017-09-11 10:16:18 +0800

    添加了最基本表单

--- src/yin/yin.module.ts
+++ src/yin/yin.module.ts
@@ -2,11 +2,12 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ButtonComponent } from './components/button';
 import { ButtonMiniComponent } from './components/button-mini';
+import { InputComponent } from './components/input';
 
 @NgModule({
-    declarations: [ ButtonComponent, ButtonMiniComponent ],
+    declarations: [ ButtonComponent, ButtonMiniComponent, InputComponent ],
     imports: [ CommonModule ],
-    exports: [ ButtonComponent, ButtonMiniComponent ],
+    exports: [ ButtonComponent, ButtonMiniComponent, InputComponent ],
     providers: [],
 })
 export class YINModule {}
